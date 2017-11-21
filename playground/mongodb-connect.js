e0b8dcb0f7ceb26232bea38f7ee0f531fd91f239
Dennis Mullen <dkmullen@gmail.com>
2017-11-21 17:16:20 -0500

    Build in add and search features

--- playground/mongodb-connect.js
+++ playground/mongodb-connect.js
@@ -1,7 +1,9 @@
 /*jshint esversion: 6 */
 
 // specifiy the MongoClient feature of the larger mongo package
-const MongoClient = require('mongodb').MongoClient;
+// const MongoClient = require('mongodb').MongoClient;
+// Below is ES6 destructuring  - This command, with only the MongoClient property, is equivelent to the line above
+const {MongoClient, ObjectID} = require('mongodb');
 
 // In mongo, you can connect to a db that you haven't created yet;
 // Mongo creates it when you add a document
@@ -22,16 +24,17 @@ MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, db) => {
   //   console.log(JSON.stringify(result.ops, undefined, 2));
   // });
 
-  db.collection('Users').insertOne({
-    name: 'Joker',
-    age: 19,
-    location: 'Da Nang'
-  }, (err, result) => {
-    if (err) {
-      return console.log('Unable to create user', err);
-    }
-    console.log(JSON.stringify(result.ops, undefined, 2));
-  });
+  // db.collection('Users').insertOne({
+  //   name: 'Rafterman',
+  //   age: 20,
+  //   location: 'Da Nang'
+  // }, (err, result) => {
+  //   if (err) {
+  //     return console.log('Unable to create user', err);
+  //   }
+  //   // result.ops is every doc created in this function
+  //   console.log(JSON.stringify(result.ops[0]._id.getTimestamp(), undefined, 2));
+  // });
 
-  db.close();
+db.close();
 });
